6a4d44e73500d066fb937c64946ffbb0f223bc60
RaBolivar <rachelbolivar01@gmail.com>
2022-04-25 21:29:07 -0600

    Update perform method to fetch url tittle and update the record in the database for the specific object

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,11 @@
+require 'open-uri'
+
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
-
+    short_url = ShortUrl.find(short_url_id)
+    title = open(short_url.full_url).read.scan(/<title>(.*?)<\/title>/)
+    short_url.update(title: title[0][0])
   end
 end
